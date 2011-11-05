ecb44023009eb7b137c52bad9fd58f992c6d9b15
Wynn Netherland <wynn.netherland@gmail.com>
2011-11-05 13:48:26 -0500

    Sets up static file serving from <theme>/public folder

--- fumblr.rb
+++ fumblr.rb
@@ -4,6 +4,7 @@ require 'thimblr/parser'
 
 config_file 'config/fumblr.yml'
 
+set :public_folder, File.join( File.dirname(__FILE__), settings.themes_path, settings.theme, "public" )
 
 before do
   @parser = Thimblr::Parser.new(File.join(settings.data_path, settings.tumblr_username + '.yml'), File.join(settings.themes_path, settings.theme, 'theme.html'))
