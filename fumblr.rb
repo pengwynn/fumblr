be74a92ac097fad6c2cf41079193d1c8db650b14
Wynn Netherland <wynn.netherland@gmail.com>
2011-11-05 14:56:07 -0500

    Pulls theme asset host from theme config file

--- fumblr.rb
+++ fumblr.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
 require 'sinatra/config_file'
 require 'thimblr/parser'
+require './lib/rack/local_assets'
+
+
+set :theme_asset_host, nil
 
 config_file 'config/fumblr.yml'
+
+theme_config_path = File.join(File.dirname(__FILE__), settings.themes_path, settings.theme, "config.yml")
+config_file theme_config_path if File.exists?(theme_config_path)
+
+if settings.theme_asset_host?
+  use Rack::LocalAssets, :asset_host => 'static.thechangelog.com'
+end
 
 set :public_folder, File.join( File.dirname(__FILE__), settings.themes_path, settings.theme, "public" )
 
